19921349c2cafa5eb77b5cce94f6c886d33a881e
Noah McGraw <35411173+NoahMcGraw@users.noreply.github.com>
2018-03-10 23:20:17 -0600

    Delete lookupJs- replaced by postRoutes.js

--- lookupJs.js
+++
@@ -1,6 +0,0 @@
-$('#lookupusername').keyup(function(){
-    var name = $('#lookupusername').val();
-    $.post('/lookup.php', { name: name}, function(data){
-        $("#result").html(data);
-    });
-});

31b8475ed6b3a4ccf8eedfe2a4d766f935964421
Noah McGraw <35411173+NoahMcGraw@users.noreply.github.com>
2018-03-14 12:41:03 -0500

    Fixed issues with rmv/add; new update feature
    User can now press any button as many times as they like! Also, new 'update info' button has been added to allow users to modify existing data.

--- lookup.php
+++ lookup.php
@@ -16,8 +16,11 @@
                 echo
                 "<div class='removeBtnDiv'>
                 <input type='submit' class='removeIndBtn' name='removeIndBtn' value='Remove User' />
-                <input type='hidden' class='removeIndValue' name='removeIndValue' value='" . $eachResult['id'] . "' />
-                <input type='hidden' class='removeIndName' name='removeIndName' value='" . $eachResult['name'] . "' />
+                <input type='submit' class='updateBtn' name='updateBtn' value='Update Info' />
+                <input type='hidden' class='indValue' name='indValue' value='" . $eachResult['id'] . "' />
+                <input type='hidden' class='indName' name='indName' value='" . $eachResult['name'] . "' />
+                <input type='hidden' class='indAge' name='indAge' value='" . $eachResult['age'] . "' />
+                <input type='hidden' class='indEmail' name='indEmail' value='" . $eachResult['email'] . "' />
                 <br><br>
                 </div>";
             }
--- postRoutes.js
+++ postRoutes.js
@@ -1,22 +1,71 @@
 $(document).ready(function(){
+    function lookupUser(){
+        var name = $('#arusername').val();
+        $.post('lookup.php', { name: name}, function(data){
+            $('#result').html(data);
+            $('.removeIndBtn').click(function(){
+                var removeIndValue = $(this).parent().find("input[name = 'indValue']").val();
+                var removeIndName = $(this).parent().find("input[name = 'indName']").val();
+                $.post('removeInd.php', {removeIndName: removeIndName, removeIndValue: removeIndValue},
+                function(){
+                    lookupUser();
+                });
+            });
+            $('.updateBtn').click(function(){
+                var updateValue = $(this).parent().find("input[name = 'indValue']").val();
+                var updateName = $(this).parent().find("input[name = 'indName']").val();
+                var updateAge = $(this).parent().find("input[name = 'indAge']").val();
+                var updateEmail = $(this).parent().find("input[name = 'indEmail']").val();
+                $.post('updateForm.php', {updateValue: updateValue, updateName: updateName, updateAge: updateAge, updateEmail: updateEmail},
+                function(updateData){
+                    $('#result').html(updateData);
+                    $('.updateSubmitBtn').click(function(){
+                        var updateSubmitValue = $(this).parent().find("input[name = 'updateValue']").val();
+                        var updateSubmitName = $(this).parent().find("input[name = 'updateName']").val();
+                        var updateSubmitAge = $(this).parent().find("input[name = 'updateAge']").val();
+                        var updateSubmitEmail = $(this).parent().find("input[name = 'updateEmail']").val();
+                        $.post('updateSubmit.php', {updateSubmitValue: updateSubmitValue, updateSubmitName: updateSubmitName,
+                        updateSubmitAge: updateSubmitAge, updateSubmitEmail: updateSubmitEmail}, function(){
+                            lookupUser();
+                        });
+                    })
+                })
+            });
+        });
+    }
 //Route to lookup and removeInd
     $('#arusername').keyup(function(){
         var name = $('#arusername').val();
         $.post('lookup.php', { name: name}, function(data){
             $('#result').html(data);
             $('.removeIndBtn').click(function(){
-                var removeIndValue = $(this).parent().find("input[name = 'removeIndValue']").val();
-                var removeIndName = $(this).parent().find("input[name = 'removeIndName']").val();
+                var removeIndValue = $(this).parent().find("input[name = 'indValue']").val();
+                var removeIndName = $(this).parent().find("input[name = 'indName']").val();
                 $.post('removeInd.php', {removeIndName: removeIndName, removeIndValue: removeIndValue},
                 function(){
-                    var name = $('#arusername').val();
-                    //$('#result').html(data);
-                    $.post('lookup.php', {name: name}, function(data){
-                        $('#result').html(data);
-                    });
+                    lookupUser();
                 });
             });
-            
+            $('.updateBtn').click(function(){
+                var updateValue = $(this).parent().find("input[name = 'indValue']").val();
+                var updateName = $(this).parent().find("input[name = 'indName']").val();
+                var updateAge = $(this).parent().find("input[name = 'indAge']").val();
+                var updateEmail = $(this).parent().find("input[name = 'indEmail']").val();
+                $.post('updateForm.php', {updateValue: updateValue, updateName: updateName, updateAge: updateAge, updateEmail: updateEmail},
+                function(updateData){
+                    $('#result').html(updateData);
+                    $('.updateSubmitBtn').click(function(){
+                        var updateSubmitValue = $(this).parent().find("input[name = 'updateValue']").val();
+                        var updateSubmitName = $(this).parent().find("input[name = 'updateName']").val();
+                        var updateSubmitAge = $(this).parent().find("input[name = 'updateAge']").val();
+                        var updateSubmitEmail = $(this).parent().find("input[name = 'updateEmail']").val();
+                        $.post('updateSubmit.php', {updateSubmitValue: updateSubmitValue, updateSubmitName: updateSubmitName,
+                        updateSubmitAge: updateSubmitAge, updateSubmitEmail: updateSubmitEmail}, function(){
+                            lookupUser();
+                        });
+                    })
+                })
+            });
         });
     });
 //Route to add
@@ -28,10 +77,7 @@ $(document).ready(function(){
             if(typeof data !== "undefined" && data.length){
             alert(data);
             }
-            var name = $('#arusername').val();
-            $.post('lookup.php', {name: name}, function(data){
-                $('#result').html(data);
-            });
+            lookupUser();
         });
     });
 });
